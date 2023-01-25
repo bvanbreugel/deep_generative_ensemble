39ac864b22717dccecf225b5e64773c648004a4a
Boris van Breugel <48678682+bvanbreugel@users.noreply.github.com>
2023-01-25 20:22:01 +0000

    Delete update.sh

--- update.sh
+++
@@ -1,5 +0,0 @@
-rsync -e ssh -rtuv ~/synthcity/workspace/* bv292.gpu2@10.147.17.202:~/uncertainty/workspace
-rsync -e ssh -rtuv bv292.gpu2@10.147.17.202:~/uncertainty/workspace/* ~/synthcity/workspace
-
-rsync -e ssh -rtuv ~/synthcity/synthetic_data/* bv292.gpu2@10.147.17.202:~/uncertainty/synthetic_data
-rsync -e ssh -rtuv bv292.gpu2@10.147.17.202:~/uncertainty/synthetic_data/* ~/synthcity/synthetic_data
